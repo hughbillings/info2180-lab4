12ed68352a6d7a322359491a26f5aa0f17f81d45
hughbillings <36485620+hughbillings@users.noreply.github.com>
2018-10-22 14:44:32 -0400

    Add Comments

--- maze.js
+++ maze.js
@@ -1,16 +1,27 @@
 function init (){
-	var walls = document.getElementById('maze').querySelectorAll('.boundary');
+	var walls = document.getElementById('maze').querySelectorAll('.boundary')
 	var s = document.getElementById('start')
 	var e = document.getElementById('end');
 	var update = document.getElementById("status");
 	var maze = document.getElementById('maze');
+
+
+
+	// Function You Lose is used when the game starts but the player exits the maze
 
 	function youLose(){
 		update.innerHTML = "CHEATER; YOU LOSE";
 		for (let i = 0; i<=walls.length; i++){
 			walls[i].style.background = 'red';
 		}
-		}
+
+	}
+
+
+
+
+
+	// The following for loop adds an event listener to all walls.
 
 		for (let i = 0; i<=walls.length; i++){
 			walls[i].addEventListener('mouseover', function(event){
@@ -24,10 +35,17 @@ function init (){
 			},false);
 
 
+	// Adding event listeners to the Start Button
+
 		s.addEventListener('mouseover', function(event){
 					
 			maze.addEventListener('mouseleave',function(event){
-				youLose();
+				if(update.innerHTML == "You Win"){
+					update.innerHTML == "Want to Play Again?";
+				} else {
+					youLose();
+				}
+				
 			},false);
 					
 		},false);
@@ -39,6 +57,11 @@ function init (){
 			}
 
 		});
+
+
+
+
+	// Adding Event listeners to the End Button
 
 		e.addEventListener('mouseover', function(event){
 			if(walls[0].style.background.length > 1){
